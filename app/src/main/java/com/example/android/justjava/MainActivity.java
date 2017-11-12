df2d05680b4da34571f58607b86069aae542b30a
Ardiono Roma <roma058a@gmail.com>
2017-11-12 16:15:29 +0700

    Just Java, Kopi sampai order beres.

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.android.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    int quantity = 2;
+
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        if (quantity < 0) {
+            quantity = 0;
+        }
+        display(quantity);
+    }
+
+    public void submitOrder(View view) {
+//        display(quantity);
+        displayPrice(quantity * 5);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}
